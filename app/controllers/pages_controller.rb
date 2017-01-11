b087a6f0bdb5e9f7ac5ce381d309be5fd95b8c38
dianacamacho <dcam1901@yahoo.com>
2017-01-11 17:28:23 -0600

    edit route and action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def show
   end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'pages#index'
   get '/show' => 'pages#show'
+  get '/edit' => 'pages#edit'
 end
